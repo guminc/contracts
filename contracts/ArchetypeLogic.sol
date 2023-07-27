ef49c5cb543c80580d6b9d913441652ef537eabb
pop-punk <harrison@gaslite.gg>
2023-07-26 23:45:42 -0400

    fix: revert platform & batch addresses

--- contracts/ArchetypeLogic.sol
+++ contracts/ArchetypeLogic.sol
@@ -126,8 +126,8 @@ struct BurnConfig {
   uint64 limit;
 }
 
-address constant PLATFORM = 0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC;
-address constant BATCH = 0x5FbDB2315678afecb367f032d93F642f64180aa3;
+address constant PLATFORM = 0x86B82972282Dd22348374bC63fd21620F7ED847B;
+address constant BATCH = 0x6Bc558A6DC48dEfa0e7022713c23D65Ab26e4Fa7;
 uint16 constant MAXBPS = 5000; // max fee or discount is 50%
 
 library ArchetypeLogic {
