dd2b267dcef253e9a010e1599f182e9ba23089c7
kneelsdev <sussykneelsnft@protonmail.com>
2023-09-29 12:04:39 -0400

    nareph linear curve implementation
    commit ee25439c18e110ef6de7f8566d391dcf544fcb1b
    Author: Blaise SIANI <bsiani@iotaorigin.de>
    Date:   Mon Aug 21 15:27:15 2023 +0000
    
        reset PLATFORM and BATCH address
    
    commit e6c0d1a91c3df2b8ef967d96dd626b571252bcf2
    Author: Blaise SIANI <bsiani@iotaorigin.de>
    Date:   Mon Aug 21 15:10:33 2023 +0000
    
        add linear curve test
    
    commit 95cc84c4a7a9a9ed03686b974fbb42b82256368a
    Author: Nareph <nmenadjou@iotaorigin.de>
    Date:   Mon Aug 21 13:56:07 2023 +0100
    
        update linear curve logic
    
    commit 8589d4ffa8045c9be8e18f593b12b1a0d9ffec5e
    Author: Nareph <nmenadjou@iotaorigin.de>
    Date:   Fri Aug 18 13:50:18 2023 +0100
    
        add the linear curve for our collection (Snip-universe)

--- contracts/ArchetypeLogic.sol
+++ contracts/ArchetypeLogic.sol
@@ -144,6 +144,7 @@ library ArchetypeLogic {
     DutchInvite storage invite,
     Discount storage discounts,
     uint256 numTokens,
+    uint256 listSupply,
     bool affiliateUsed
   ) public view returns (uint256) {
     uint256 price = invite.price;
@@ -165,6 +166,11 @@ library ArchetypeLogic {
     }
 
     uint256 cost = price * numTokens;
+    
+    if(invite.interval == 0){ //Apply the linear curve 
+      uint256 lastPrice =  price + invite.delta * listSupply;
+      cost = lastPrice * numTokens + invite.delta * numTokens * (numTokens - 1) / 2;
+    }
 
     if (affiliateUsed) {
       cost = cost - ((cost * discounts.affiliateDiscount) / 10000);
@@ -248,7 +254,8 @@ library ArchetypeLogic {
       revert MaxSupplyExceeded();
     }
 
-    uint256 cost = computePrice(i, config.discounts, quantity, affiliate != address(0));
+    uint256 inviteListSupply = listSupply[auth.key];
+    uint256 cost = computePrice(i, config.discounts, quantity, inviteListSupply, affiliate != address(0));
 
     if (i.tokenAddress != address(0)) {
       IERC20Upgradeable erc20Token = IERC20Upgradeable(i.tokenAddress);
@@ -337,12 +344,13 @@ library ArchetypeLogic {
     mapping(address => OwnerBalance) storage _ownerBalance,
     mapping(address => mapping(address => uint128)) storage _affiliateBalance,
     address affiliate,
-    uint256 quantity
+    uint256 quantity,
+    uint256 listSupply
   ) public {
     address tokenAddress = i.tokenAddress;
     uint128 value = uint128(msg.value);
     if (tokenAddress != address(0)) {
-      value = uint128(computePrice(i, config.discounts, quantity, affiliate != address(0)));
+      value = uint128(computePrice(i, config.discounts, quantity, listSupply, affiliate != address(0)));
     }
 
     uint128 affiliateWad;
