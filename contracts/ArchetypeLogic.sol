a92d6b261d2c958d91d11a50a8eececc62e449b7
PolyDegen <polydegen@protonmail.com>
2023-10-09 07:59:51 +0530

    delete comment

--- contracts/ArchetypeLogic.sol
+++ contracts/ArchetypeLogic.sol
@@ -148,7 +148,7 @@ struct VrfMintInfo {
 }
 
 address constant PLATFORM = 0x86B82972282Dd22348374bC63fd21620F7ED847B;
-address constant BATCH = 0x6Bc558A6DC48dEfa0e7022713c23D65Ab26e4Fa7; // mainnet
+address constant BATCH = 0x6Bc558A6DC48dEfa0e7022713c23D65Ab26e4Fa7;
 uint16 constant MAXBPS = 5000; // max fee or discount is 50%
 
 address constant VRF_CORDINATOR = 0x271682DEB8C4E0901D1a1550aD2e64D568E69909;
