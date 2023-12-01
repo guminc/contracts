2f385e53df1392abb404779fbcfbfd3ded5343b4
CoinDegen <polydegen@protonmail.com>
2023-11-30 19:21:37 -0800

    delete redundant dependencies

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,6 +1,7 @@
 import { ethers, run } from "hardhat";
 
 async function main() {
+  // don't deploy ERC20 token on mainnet
   const AlienMiladyFumo = await ethers.getContractFactory("AlienMiladyFumo");
   const alienMiladyFumoContract = await AlienMiladyFumo.deploy();
   await alienMiladyFumoContract.deployed();
@@ -9,22 +10,16 @@ async function main() {
   const fumoErc20Address = alienMiladyFumoContract.address; // use this on Sepolia
   // const fumoErc20Address = "0x2890df158d76e584877a1d17a85fea3aeeb85aa6" // use this on mainnet
 
-  const NFT = await ethers.getContractFactory("FUMOMintable");
-
   const placeholderAddress = "0x60A59d7003345843BE285c15c7C78B62b61e0d7c";
 
+  const NFT = await ethers.getContractFactory("FUMOMintable");
   const nftContract = await NFT.deploy(placeholderAddress); // this minter param is a placeholder, we'll replace it after we deploy Redeemer
-
   await nftContract.deployed();
-
   console.log("NFT Contract deployed to:", nftContract.address);
 
   const Redeemer = await ethers.getContractFactory("Redeemer");
-
   const redeemerContract = await Redeemer.deploy(fumoErc20Address, nftContract.address);
-
   await redeemerContract.deployed();
-
   console.log("Redeemer Contract deployed to:", redeemerContract.address);
 
   await nftContract.setMinter(redeemerContract.address);
