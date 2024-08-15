6fe12b33196aa3f6a127b25c6209b6f1be896a00
kneelsdev <sussykneelsnft@protonmail.com>
2024-08-14 21:48:55 -0400

    fix default network

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -43,7 +43,7 @@ const config: HardhatUserConfig = {
   //     },
   //   ],
   // },
-  defaultNetwork: "arbitrum_mainnet",
+  defaultNetwork: "hardhat",
   networks: {
     sepolia: {
       accounts: [privateKey],
